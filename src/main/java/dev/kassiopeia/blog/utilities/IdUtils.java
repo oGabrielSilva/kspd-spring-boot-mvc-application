008171d3638af283fb740c2507fc784ee3b7951d
Gabriel Silva <gabriel04gh1.gh@gmail.com>
2024-05-05 19:08:12 -0300

    mail validation

--- src/main/java/dev/kassiopeia/blog/utilities/IdUtils.java
+++ src/main/java/dev/kassiopeia/blog/utilities/IdUtils.java
@@ -4,14 +4,14 @@ import java.security.SecureRandom;
 
 public class IdUtils {
     private static SecureRandom random = new SecureRandom();
-    private static String alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static char[] alf = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 
     public static String generate(int len) {
         StringBuilder code = new StringBuilder();
 
         for (int i = 0; i < len; i++) {
-            int index = random.nextInt(alf.length());
-            code.append(alf.charAt(index));
+            int index = random.nextInt(alf.length);
+            code.append(alf[index]);
         }
 
         return code.toString();
